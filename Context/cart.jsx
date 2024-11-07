47e74fabc01ea79643cfd4aac307df1f15f4ec8f
Alexis Jackson <128395355+alexismgjackson-github@users.noreply.github.com>
2024-11-07 13:56:22 -0500

    comments added to context

--- Context/cart.jsx
+++ Context/cart.jsx
@@ -3,11 +3,16 @@ import { createContext, useState, useEffect } from "react";
 export const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
+  // the initial state of the cart items will be an empty array - if items are not in browser
   const [cartItems, setCartItems] = useState(
     localStorage.getItem("cartItems")
       ? JSON.parse(localStorage.getItem("cartItems"))
       : []
   );
+
+  // checking if item is already in cart
+  // if item is already in cart then increase quantity of item in the cart
+  // if item is not in cart then add to cart
 
   const addToCart = (item) => {
     const isItemInCart = cartItems.find((cartItem) => cartItem.id === item.id);
@@ -25,6 +30,10 @@ export const CartProvider = ({ children }) => {
     }
   };
 
+  // checking if item is already in cart
+  // if the quantity of the item is equal to 1 then remove the item from cart
+  // if the quantity of the item is greater than 1 then decrease the quantity of the item in cart
+
   const removeFromCart = (item) => {
     const isItemInCart = cartItems.find((cartItem) => cartItem.id === item.id);
 
@@ -41,15 +50,21 @@ export const CartProvider = ({ children }) => {
     }
   };
 
+  // set the cart items to an empty array
+
   const clearCart = () => {
     setCartItems([]);
   };
+
+  // calculate the total price of the items in the cart
 
   const getCartTotal = () => {
     return formatUSD(
       cartItems.reduce((total, item) => total + item.price * item.quantity, 0)
     );
   };
+
+  // format item price to USD
 
   function formatUSD(num) {
     return num.toLocaleString("en-US", {
@@ -58,9 +73,13 @@ export const CartProvider = ({ children }) => {
     });
   }
 
+  // persist the cart state in the browser
+
   useEffect(() => {
     localStorage.setItem("cartItems", JSON.stringify(cartItems));
   }, [cartItems]);
+
+  // get the cart items from the browser.
 
   useEffect(() => {
     const cartItems = localStorage.getItem("cartItems");
