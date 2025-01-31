45a2cb7be904b3858047eb735c2e934c473023c7
Alexis Jackson <128395355+alexismgjackson-github@users.noreply.github.com>
2025-01-31 14:15:15 -0500

    file restructure

--- src/index.jsx
+++ src/index.jsx
@@ -10,7 +10,7 @@ import Home from "../pages/Home";
 import Layout from "../components/Layout";
 import Menu from "../pages/Menu";
 import OrderComplete from "../pages/OrderComplete";
-import "./index.css";
+import "../index.css";
 
 function App() {
   return (
