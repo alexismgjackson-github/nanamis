f586b7fee19702c404f1cb7b3ffd0581ec86e718
Alexis Jackson <128395355+alexismgjackson-github@users.noreply.github.com>
2025-04-02 14:02:43 -0400

    update cart.jsx

--- Context/cart.jsx
+++ Context/cart.jsx
@@ -11,7 +11,7 @@ export const CartProvider = ({ children }) => {
   );
 
   // if item is already in cart then increase quantity of item in the cart but
-  // if item is not in cart then add to cart
+  // if item is not in cart then add the item to cart
 
   const addToCart = (item) => {
     const isItemInCart = cartItems.find((cartItem) => cartItem.id === item.id);
@@ -32,22 +32,25 @@ export const CartProvider = ({ children }) => {
     }
   };
 
+  // if item is already in cart...
   // if the quantity of the item is equal to 1 then remove the item from cart but
   // if the quantity of the item is greater than 1 then decrease the quantity of the item in cart
 
   const removeFromCart = (item) => {
     const isItemInCart = cartItems.find((cartItem) => cartItem.id === item.id);
 
-    if (isItemInCart.quantity === 1) {
-      setCartItems(cartItems.filter((cartItem) => cartItem.id !== item.id));
-    } else {
-      setCartItems(
-        cartItems.map((cartItem) =>
-          cartItem.id === item.id
-            ? { ...cartItem, quantity: cartItem.quantity - 1 }
-            : cartItem
-        )
-      );
+    if (isItemInCart) {
+      if (isItemInCart.quantity === 1) {
+        setCartItems(cartItems.filter((cartItem) => cartItem.id !== item.id));
+      } else {
+        setCartItems(
+          cartItems.map((cartItem) =>
+            cartItem.id === item.id
+              ? { ...cartItem, quantity: cartItem.quantity - 1 }
+              : cartItem
+          )
+        );
+      }
     }
   };
 
@@ -61,9 +64,8 @@ export const CartProvider = ({ children }) => {
 
   const getCartItemTotal = (item) => {
     const isItemInCart = cartItems.find((cartItem) => cartItem.id === item.id);
-
     if (isItemInCart) {
-      return formatUSD(item.price * item.quantity);
+      return formatUSD(isItemInCart.price * isItemInCart.quantity);
     }
   };
 
@@ -86,7 +88,7 @@ export const CartProvider = ({ children }) => {
     return formatUSD(cartSubTotal * 0.09);
   };
 
-  // calculate the grand total price of the items in the cart
+  // calculate the grand total price of the items in the cart, including tax
 
   const getGrandTotal = () => {
     const cartSubTotal = cartItems.reduce(
@@ -108,20 +110,24 @@ export const CartProvider = ({ children }) => {
     });
   }
 
-  // persist the cart state in the browser
+  // localStorage handling (initialization)
+  // whenever cartItems is updated, save the changes to localStorage (if the cart is not empty)
 
   useEffect(() => {
-    localStorage.setItem("cartItems", JSON.stringify(cartItems));
-  }, [cartItems]);
-
-  // get the cart items from the browser
-
-  useEffect(() => {
-    const cartItems = localStorage.getItem("cartItems");
-    if (cartItems) {
-      setCartItems(JSON.parse(cartItems));
+    if (cartItems.length > 0) {
+      localStorage.setItem("cartItems", JSON.stringify(cartItems));
     }
-  }, []);
+  }, [cartItems]); // run the effect only when the cartItems state changes
+
+  // localStorage handling (persistence)
+  // when the component first appears on the page, attempt to load the cartItems from localStorage
+
+  useEffect(() => {
+    const storedCartItems = localStorage.getItem("cartItems");
+    if (storedCartItems) {
+      setCartItems(JSON.parse(storedCartItems));
+    }
+  }, []); // will run only once, after the initial render of the component
 
   return (
     <CartContext.Provider
@@ -136,7 +142,7 @@ export const CartProvider = ({ children }) => {
         getGrandTotal,
       }}
     >
-      {children}
+      {children}{" "}
     </CartContext.Provider>
   );
 };
